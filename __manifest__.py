44cf371fd50a95cc99241a536b42af0f5285d022
Fayis <76743743+fayisvp@users.noreply.github.com>
2023-11-29 03:54:22 -0800

    outer python files(init and manifest)
    outer init file and manifest under custom_addon

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hospital_Management',
+    'version': '1.2',
+    'category': 'Hospital managemnet system',
+    'summary': 'Hospital Management',
+    'description': """
+This module is a demo module done for a hospital management system.
+    """,
+    'author': 'Team Odoo',
+    'depends': [],
+    'sequence': -1000,
+    'data': ['views\menu.xml',
+             'views\patients.xml'
+             ],
+    'description': ['E:\fayis\Odoo 15 dev\server\odoo\custom_addons\om_hospital\static\description\icon.png'],
+    'installable' : True,
+    'auto_install' : False,
+    'license' : 'LGPL-3',
+    'application' : True,
+    'website' : 'www.aaa.com',
+}
